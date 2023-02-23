128b62d1c2f142a3b8de5fd9ab1fcaaf56efeb4e
Julia-Dantas <90366819+Julia-Dantas@users.noreply.github.com>
2023-02-23 10:41:50 -0700

    Updated the Sign up Screen
    (Almost done!)

--- crisis_connect/lib/screens/CreateProfileScreen.dart
+++ crisis_connect/lib/screens/CreateProfileScreen.dart
@@ -31,12 +31,12 @@ class CreateProfile extends State<FormScreen> {
 
   // List of countries:
   late final List<String> _countries = ['Afghanistan', 'Aland Islands', 'Albania', 'Algeria', 'American Samoa', 'Andorra', 'Angola', 'Anguilla', 'Antarctica', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia, Plurinational State of', 'Bonaire, Sint Eustatius and Saba', 'Bosnia and Herzegovina', 'Botswana', 'Bouvet Island', 'Brazil', 'British Indian Ocean Territory', 'Brunei Darussalam', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Christmas Island', 'Cocos (Keeling) Islands', 'Colombia', 'Comoros', 'Congo', 'Congo, The Democratic Republic of the', 'Cook Islands', 'Costa Rica', "Côte d'Ivoire", 'Croatia', 'Cuba', 'Curaçao', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Falkland Islands (Malvinas)', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Guiana', 'French Polynesia', 'French Southern Territories', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guadeloupe', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Heard Island and McDonald Islands', 'Holy See (Vatican City State)', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran, Islamic Republic of', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', "Korea, Democratic People's Republic of", 'Korea, Republic of', 'Kuwait', 'Kyrgyzstan', "Lao People's Democratic Republic", 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macao', 'Macedonia, Republic of', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Martinique', 'Mauritania', 'Mauritius', 'Mayotte', 'Mexico', 'Micronesia, Federated States of', 'Moldova, Republic of', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue', 'Norfolk Island', 'Northern Mariana Islands', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Palestinian Territory, Occupied', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Pitcairn', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Réunion', 'Romania', 'Russian Federation', 'Rwanda', 'Saint Barthélemy', 'Saint Helena, Ascension and Tristan da Cunha', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Martin (French part)', 'Saint Pierre and Miquelon', 'Saint Vincent and the Grenadines', 'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Sint Maarten (Dutch part)', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Georgia and the South Sandwich Islands', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'South Sudan', 'Svalbard and Jan Mayen', 'Swaziland', 'Sweden', 'Switzerland', 'Syrian Arab Republic', 'Taiwan, Province of China', 'Tajikistan', 'Tanzania, United Republic of', 'Thailand', 'Timor-Leste', 'Togo', 'Tokelau', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks and Caicos Islands', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'United States Minor Outlying Islands', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Venezuela, Bolivarian Republic of', 'Viet Nam', 'Virgin Islands, British', 'Virgin Islands, U.S.', 'Wallis and Futuna', 'Yemen', 'Zambia', 'Zimbabwe'];
-  late String _country;
+  late String _country = 'Country';
 
+  final _controller = TextEditingController();
 
-  var _controller = TextEditingController();
-
-
+  // box radius
+  final double _boxRadius = 8;
 
   void _toggle() {
     setState(() {
@@ -47,137 +47,263 @@ class CreateProfile extends State<FormScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Widget _header(String title) {
-    return TextField(
-      decoration: InputDecoration(labelText: title),
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+          child: Text(
+            title,
+            textAlign: TextAlign.left,
+            overflow: TextOverflow.ellipsis,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          )
+      ),
     );
   }
 
   Widget _buildFirstName() {
-    return TextFormField(
-      decoration: const InputDecoration(labelText: 'First Name'),
-      //maxLength: 10,
-      validator: (String? value) {
-        return value==null ||value.isEmpty ? "First Name can't be empty" : null;
-      },
-      onSaved: (String? value) {
-        _firstname = value!;
-      },
-    );
+    return
+      Container(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(_boxRadius),
+            color: Colors.white,
+          ),
+          child : TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'First Name',
+              border: InputBorder.none,
+            ),
+            //maxLength: 10,
+            validator: (String? value) {
+              return value==null ||value.isEmpty ? "First name can't be empty" : null;
+            },
+            onSaved: (String? value) {
+              _firstname = value!;
+            },
+          ));
   }
 
   Widget _buildLastName() {
-    return TextFormField(
-      decoration: const InputDecoration(labelText: 'Last Name'),
-      //maxLength: 10,
-      validator: (String? value) {
-        return value==null ||value.isEmpty ? "Last Name can't be empty" : null;
-      },
-      onSaved: (String? value) {
-        _lastname = value!;
-      },
-    );
+    return
+      Container(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(_boxRadius),
+            color: Colors.white,
+          ),
+          child : TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Last Name',
+              border: InputBorder.none,
+            ),
+            //maxLength: 10,
+            validator: (String? value) {
+              return value==null ||value.isEmpty ? "Last name can't be empty" : null;
+            },
+            onSaved: (String? value) {
+              _lastname = value!;
+            },
+          ));
   }
 
   Widget _buildEmail() {
-    return TextFormField(
-      decoration: const InputDecoration(labelText: 'Email'),
-      validator: (String? value) {
-        return value==null ||value.isEmpty ? "Email can't be empty" : null;
-      },
-      onSaved: (String? value) {
-        _email = value!;
-      },
-    );
+    return
+      Container(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(_boxRadius),
+            color: Colors.white,
+          ),
+          child : TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Email',
+              border: InputBorder.none,
+            ),
+            validator: (String? value) {
+              return value==null ||value.isEmpty ? "Email can't be empty" : null;
+            },
+            onSaved: (String? value) {
+              _email = value!;
+            },
+          ));
   }
 
   Widget _buildPassword() {
-    return TextFormField(
-      controller: _controller,
-      decoration: InputDecoration(
-          labelText: 'Password',
-          suffixIcon: IconButton(
-              onPressed: () { _toggle(); },
-              icon: Icon(Icons.remove_red_eye)),
-      ),
-      obscureText: _obscureText,
-      validator: (String? value) {
-        return value==null ||value.isEmpty ? "Password can't be empty" : null;
-      },
-      onSaved: (String? value) {
-        _password = value!;
-      },
-    );
-  }
-
-  Widget _hideShowPassword() {
-    return TextButton(
-        onPressed: _toggle,
-        child: Text(_obscureText ? "Show" : "Hide"));
+    return
+      Container(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(_boxRadius),
+            color: Colors.white,
+          ),
+          child : TextFormField(
+            controller: _controller,
+            decoration: InputDecoration(
+              hintText: 'Password',
+              border: InputBorder.none,
+              suffixIcon: IconButton(
+                  onPressed: () { _toggle(); },
+                  icon: Icon(Icons.remove_red_eye)),
+            ),
+            obscureText: _obscureText,
+            validator: (String? value) {
+              return value==null ||value.isEmpty ? "Password can't be empty" : null;
+            },
+            onSaved: (String? value) {
+              _password = value!;
+            },
+          ));
   }
 
   Widget _buildPhoneNumber() {
-    return TextFormField(
-      decoration: const InputDecoration(labelText: 'Phone number'),
-      validator: (String? value) {
-        return value==null ||value.isEmpty ? "Phone number can't be empty" : null;
-      },
-      onSaved: (String? value) {
-        _phoneNumber = value!;
-      },
-    );
+    return
+      Container(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(_boxRadius),
+            color: Colors.white,
+          ),
+          child : TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Phone number',
+              border: InputBorder.none,
+            ),
+            validator: (String? value) {
+              return value==null ||value.isEmpty ? "Phone number can't be empty" : null;
+            },
+            onSaved: (String? value) {
+              _phoneNumber = value!;
+            },
+          ));
   }
 
   Widget _buildEmergencyFirstName() {
-    return TextFormField(
-      decoration: const InputDecoration(labelText: 'Emergency Contact First Name'),
-      //maxLength: 10,
-      validator: (String? value) {
-        return value==null ||value.isEmpty ? "Emergency Contact First Name can't be empty" : null;
-      },
-      onSaved: (String? value) {
-        _firstname = value!;
-      },
-    );
+    return
+      Container(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(_boxRadius),
+            color: Colors.white,
+          ),
+          child : TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Emergency Contact First Name',
+              border: InputBorder.none,
+            ),
+            //maxLength: 10,
+            validator: (String? value) {
+              return value==null ||value.isEmpty ? "Emergency contact first name can't be empty" : null;
+            },
+            onSaved: (String? value) {
+              _firstname = value!;
+            },
+          ));
   }
 
   Widget _buildEmergencyLastName() {
-    return TextFormField(
-      decoration: const InputDecoration(labelText: 'Emergency Contact Last Name'),
-      //maxLength: 10,
-      validator: (String? value) {
-        return value==null ||value.isEmpty ? "Emergency Contact Last Name can't be empty" : null;
-      },
-      onSaved: (String? value) {
-        _lastname = value!;
-      },
-    );
+    return
+      Container(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(_boxRadius),
+            color: Colors.white,
+          ),
+          child : TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Emergency Contact Last Name',
+              border: InputBorder.none,
+            ),
+            //maxLength: 10,
+            validator: (String? value) {
+              return value==null ||value.isEmpty ? "Emergency contact last name can't be empty" : null;
+            },
+            onSaved: (String? value) {
+              _lastname = value!;
+            },
+          ));
   }
 
   Widget _buildEmergencyPhoneNumber() {
-    return TextFormField(
-      decoration: const InputDecoration(labelText: 'Emergency Contact Phone number'),
-      validator: (String? value) {
-        return value==null ||value.isEmpty ? "Emergency Phone number can't be empty" : null;
-      },
-      onSaved: (String? value) {
-        _phoneNumber = value!;
-      },
-    );
+    return
+      Container(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(_boxRadius),
+            color: Colors.white,
+          ),
+          child : TextFormField(
+            decoration: const InputDecoration(
+                hintText: 'Emergency Contact Phone number',
+                border: InputBorder.none,
+            ),
+            validator: (String? value) {
+              return value==null ||value.isEmpty ? "Emergency phone number can't be empty" : null;
+            },
+            onSaved: (String? value) {
+              _phoneNumber = value!;
+            },
+          )
+      );
   }
 
   Widget _buildCountryDropdown() {
-    return DropdownButton<String>(
-      items: _countries.map((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-      onChanged: (String? value) {
-        _country = value!;
-      }
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(_boxRadius),
+        color: Colors.white,
+      ),
+      child: DropdownButtonHideUnderline(
+          child: DropdownButton(
+            hint: _country == null
+                ? Text('Country') :
+            Text(
+              _country,
+              style: const TextStyle(
+                fontSize: 16,
+                color: Color.fromRGBO(117, 117, 117, 1),
+              ),
+            ),
+            isExpanded: true,
+            iconSize: 30.0,
+            style: const TextStyle(
+              fontSize: 16,
+              color: Color.fromRGBO(117, 117, 117, 1),
+            ),
+            items: _countries.map((val) {
+              return DropdownMenuItem<String>(
+                value: val,
+                child: Text(val),
+              );
+            },
+            ).toList(),
+            onChanged: (val) {
+              setState(() {
+                _country = val!;
+              },
+              );
+            },
+          ) // your Dropdown Widget here
+      ),
     );
   }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -192,20 +318,30 @@ class CreateProfile extends State<FormScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 _header('Contact Information'),
+                SizedBox(height: 10),
                 _buildFirstName(),
+                SizedBox(height: 10),
                 _buildLastName(),
+                SizedBox(height: 10),
                 _buildEmail(),
+                SizedBox(height: 10),
                 _buildPassword(),
+                SizedBox(height: 10),
                 _buildPhoneNumber(),
+                SizedBox(height: 10),
                 _buildCountryDropdown(),
+                SizedBox(height: 10),
                 _header('Emergency Contact Information'),
+                SizedBox(height: 10),
                 _buildEmergencyFirstName(),
+                SizedBox(height: 10),
                 _buildEmergencyLastName(),
+                SizedBox(height: 10),
                 _buildEmergencyPhoneNumber(),
-                SizedBox(height: 100),
+                SizedBox(height: 10),
                 ElevatedButton(
-                  child: Text(
-                    'Submit',
+                  child: const Text(
+                    'Create Account',
                     style: TextStyle(color: Colors.white, fontSize: 16),
                   ),
                   onPressed: () {

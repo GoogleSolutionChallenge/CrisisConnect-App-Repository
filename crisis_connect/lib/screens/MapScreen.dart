91c6f8d0b74b52c165d17f5c8fb23e2f8bda44c6
Julia-Dantas <90366819+Julia-Dantas@users.noreply.github.com>
2023-03-05 10:15:38 -0700

    Update MapScreen.dart

--- crisis_connect/lib/screens/MapScreen.dart
+++ crisis_connect/lib/screens/MapScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart' show Location, LocationData;
+import 'package:google_maps_webservice/places.dart' as places;
+import 'package:location/location.dart' as location;
+import 'package:location/location.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -9,14 +11,18 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  LatLng _initialcameraposition = LatLng(20.5937, 78.9629);
+  final LatLng _initialcameraposition = const LatLng(53.5461, 113.4937);
   late GoogleMapController _controller;
-  Location _location = Location();
+
+  final location.Location _location = location.Location();
+
+  Set<Marker> _markers = {};
+  final places.GoogleMapsPlaces _places = places.GoogleMapsPlaces(apiKey: 'AIzaSyA7guPDiP0D6f8QD-gm-Oxc-mOLVcDpijQ');
 
   void _onMapCreated(GoogleMapController _cntlr)
   {
     _controller = _cntlr;
-    _location.onLocationChanged.listen((LocationData l) {
+    _location.onLocationChanged.listen((location.LocationData l) {
       _controller.animateCamera(
         CameraUpdate.newCameraPosition(
           CameraPosition(target: LatLng(l.latitude!, l.longitude!),zoom: 15),
@@ -25,13 +31,34 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Future<void> _performSearch(String query) async {
+    final LocationData location = await _location.getLocation();
+    final places.PlacesSearchResponse result = await _places.searchByText(query, location: places.Location(lat: location.latitude!, lng: location.longitude!));
+
+    if (result.status == "OK") {
+      // Create markers for each hospital result
+      final markers = result.results.map((place) => Marker(
+        markerId: MarkerId(place.id!),
+        position: LatLng(place.geometry?.location.lat ?? 0, place.geometry?.location.lng ?? 0),
+        infoWindow: InfoWindow(title: place.name ?? ''),
+      ));
+
+
+      // Update map markers with search results
+      setState(() {
+        _markers = Set<Marker>.of(markers);
+      });
+    } else {
+      print("Error: ${result.errorMessage}");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text("Map"),
-          centerTitle: true,
+        title: Text("Map"),
+        centerTitle: true,
       ),
       body: Container(
         height: MediaQuery.of(context).size.height,
@@ -39,10 +66,11 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Stack(
           children: [
             GoogleMap(
-              initialCameraPosition: CameraPosition(target: _initialcameraposition),
+              initialCameraPosition: CameraPosition(target: _initialcameraposition, zoom: 14),
               mapType: MapType.normal,
               onMapCreated: _onMapCreated,
               myLocationEnabled: true,
+              markers: _markers,
             ),
           ],
         ),
